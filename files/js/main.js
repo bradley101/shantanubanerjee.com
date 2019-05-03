438b46355b7df1ff1166b126debefa4b0800ec88
bradley101 <shantanu.banerjee.vt@gmail.com>
2019-05-03 13:23:21 +0530

    grid changes

--- files/js/main.js
+++ files/js/main.js
@@ -10,7 +10,23 @@
 		await sleep(500);
 		span.innerHTML = '|';
 		await sleep(500);
-		span.innerHTML = 'H';
+		span.innerHTML = 'p';
+		await sleep(200);
+		span.innerHTML += 'r';
+		await sleep(200);
+		span.innerHTML += 'i';
+		await sleep(200);
+		span.innerHTML += 'n';
+		await sleep(300);
+		span.innerHTML += 't';
+		await sleep(300);
+		span.innerHTML += 'f';
+		await sleep(200);
+		span.innerHTML += '(';
+		await sleep(200);
+		span.innerHTML += '"';
+		await sleep(200);
+		span.innerHTML += 'H';
 		await sleep(200);
 		span.innerHTML += 'e';
 		await sleep(200);
@@ -24,11 +40,11 @@
 		await sleep(300);
 		span.innerHTML += '|';
 		await sleep(500);
-		span.innerHTML = 'Hello&nbsp;';
+		span.innerHTML = 'printf("Hello&nbsp;';
 		await sleep(500);
 		span.innerHTML += '|';
 		await sleep(500);
-		span.innerHTML = 'Hello&nbsp;';
+		span.innerHTML = 'printf("Hello&nbsp;';
 		span.innerHTML += 'W';
 		await sleep(200);
 		span.innerHTML += 'o';
@@ -40,10 +56,16 @@
 		span.innerHTML += 'd';
 		await sleep(300);
 		span.innerHTML += '!';
+		await sleep(200);
+		span.innerHTML += '"';
+		await sleep(200);
+		span.innerHTML += ')';
+		await sleep(200);
+		span.innerHTML += ';';
 	};
 	function setRepeat() {
 		blink();
-		setInterval(blink, 10000);
+		setInterval(blink, 15000);
 	}
 	let init = () => {
 		setRepeat();
