0ada158f985ebdc25da03e8639537c539e7a5228
Shantanu Banerjee <Shantanu.Banerjee@veritas.com>
2020-04-21 23:56:07 +0530

    enable cors in cloud

--- triggers/functions/index.js
+++ triggers/functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const serviceAccount = require(__dirname + '/shantanubanerjeedotcom-firebase-adminsdk-ghboc-956bd759c8.json');
+const cors = require('cors') ({origin: true});
 let _ = class {};
 
 admin.initializeApp({
