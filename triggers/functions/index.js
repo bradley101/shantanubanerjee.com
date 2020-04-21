8a297c990348b60b115663166173b500654f2a23
Shantanu Banerjee <Shantanu.Banerjee@veritas.com>
2020-04-21 23:52:28 +0530

    created backend trigger

---
+++ triggers/functions/index.js
@@ -0,0 +1,31 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const serviceAccount = require(__dirname + '/shantanubanerjeedotcom-firebase-adminsdk-ghboc-956bd759c8.json');
+let _ = class {};
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'shantanubanerjeedotcom.firebaseio.com'
+});
+
+const db = admin.firestore();
+
+exports.dbTrigger = functions.firestore.document('static/type1').onWrite((change, context) => {
+    const d = change.after.data();
+    _.response.about = d.about;
+    _.response.skills = d.skills;
+    console.log(change.after.data());
+    return true;
+})
+
+exports.getInnerHtml = functions.https.onRequest((req, res) => {
+    db.collection('static').doc('type1').get().then(doc => {
+        if (doc.exists) {
+            res.status(200).contentType('application/json').send(doc.data());
+        }
+        return true;
+    }).catch(err => {
+        console.log(err);
+    })
+})
+ 
