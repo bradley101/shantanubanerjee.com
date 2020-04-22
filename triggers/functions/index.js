5f2c7cef2360f41df175bd293913eb1340d0a974
Shantanu Banerjee <Shantanu.Banerjee@veritas.com>
2020-04-22 12:14:42 +0530

    cors

--- triggers/functions/index.js
+++ triggers/functions/index.js
@@ -1,8 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const serviceAccount = require(__dirname + '/shantanubanerjeedotcom-firebase-adminsdk-ghboc-956bd759c8.json');
-const cors = require('cors') ({origin: true});
-let _ = class {};
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
@@ -10,14 +8,6 @@ admin.initializeApp({
 });
 
 const db = admin.firestore();
-
-exports.dbTrigger = functions.firestore.document('static/type1').onWrite((change, context) => {
-    const d = change.after.data();
-    _.response.about = d.about;
-    _.response.skills = d.skills;
-    console.log(change.after.data());
-    return true;
-})
 
 exports.getInnerHtml = functions.https.onRequest((req, res) => {
     db.collection('static').doc('type1').get().then(doc => {
