efd4dd1b65a29e9d1305d4f429597b411c53a81a
Shantanu Banerjee <Shantanu.Banerjee@veritas.com>
2020-04-22 12:53:33 +0530

    added analytics

---
+++ files/js/pre.js
@@ -0,0 +1,15 @@
+(() => {
+    var firebaseConfig = {
+        apiKey: "AIzaSyBxQqY-KhJM4ldagrgDHiMgUHotR4xr_50",
+        authDomain: "shantanubanerjeedotcom.firebaseapp.com",
+        databaseURL: "https://shantanubanerjeedotcom.firebaseio.com",
+        projectId: "shantanubanerjeedotcom",
+        storageBucket: "shantanubanerjeedotcom.appspot.com",
+        messagingSenderId: "523346720146",
+        appId: "1:523346720146:web:c964821947cd4004ba3d4b",
+        measurementId: "G-GX28YCNXM9"
+    };
+    
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+})();
